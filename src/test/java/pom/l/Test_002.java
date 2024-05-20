683173c6a296cd4dff6f3c285fd265e366e9311f
example-id123 <167778296+example-id123@users.noreply.github.com>
2024-05-20 21:22:39 +0530

    Update Test_002.java

--- src/test/java/pom/l/Test_002.java
+++ src/test/java/pom/l/Test_002.java
@@ -4,6 +4,8 @@ public class Test_002 {
 
 	public static void main(String[] args) {
 		System.out.println("Test_002");
+		System.out.println("New changes");
+		System.out.println("must implemented");
 
 	}
 
